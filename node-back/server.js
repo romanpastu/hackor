d7a72de40ab0c8f12a027b747d5652cf61e99e61
romanpastu <roman.pastushenko.1997@gmail.com>
2020-06-27 18:30:02 +0200

    mongo connection

--- node-back/server.js
+++ node-back/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const morgan= require('morgan')
-const bodyParser= require('body-parser')
-const cors= require('cors')
-const mongoose= require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 
 const app = express()
+
+//db
+mongoose.connect(process.env.DATABASE_CLOUD, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log(err))
 
 //import routes
 const authRoutes = require('./routes/auth')
@@ -14,7 +19,7 @@ const authRoutes = require('./routes/auth')
 //app middlewares
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-app.use(cors({origin: process.env.CLIENT_URL}))
+app.use(cors({ origin: process.env.CLIENT_URL }))
 
 
 //middlewares
