286f387798961a481ced85ff2086388e10de8ef9
romanpastu <roman.pastushenko.1997@gmail.com>
2020-07-05 21:58:56 +0200

    user confirmation implementation

--- node-back/controllers/auth.js
+++ node-back/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 const AWS = require('aws-sdk')
 const jwt = require('jsonwebtoken')
 const {registerEmailParams} = require('../helpers/email')
+const shortId = require('shortid')
 
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -48,4 +49,40 @@ exports.register = (req, res) => {
     })
 
 
+}
+
+exports.registerActivate = (req, res) => {
+    const {token} = req.body
+    // console.log(token)
+    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(err, decoded){
+        if(err) {
+            return res.status(401).json({
+                error: 'Expired link. Try again'
+            })
+        }
+
+        const {name, email, password} = jwt.decode(token)
+        const username = shortId.generate()
+
+        User.findOne({email}).exec((err, user) =>{
+            if(user){
+                return res.status(401).json({
+                    error: 'Email is taken'
+                })
+            }
+
+            //register new user
+            const newUser = new User({username, name, email, password})
+            newUser.save((err, result) => {
+                if(err) {
+                    return res.status(401).json({
+                        error: 'Error saving user in database. Try later'
+                    })
+                }
+                return res.json({
+                    message: 'Registration success. Please login'
+                })
+            })
+        })
+    })
 }
