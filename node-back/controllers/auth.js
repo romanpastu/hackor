4fe8f185b4a4ebecab593e0454bb4b4fccbc2423
romanpastu <roman.pastushenko.1997@gmail.com>
2020-07-08 20:01:11 +0200

    backend access token creation

--- node-back/controllers/auth.js
+++ node-back/controllers/auth.js
@@ -86,3 +86,30 @@ exports.registerActivate = (req, res) => {
         })
     })
 }
+
+exports.login = (req,res) => {
+    const {email, password} = req.body
+    // console.log(email, password)
+
+    User.findOne({email}).exec((err, user) =>{
+        if(err || !user){
+            return res.status(400).json({
+                error: 'User with that email does not exist. Please register'
+            })
+        }
+        //authenticate
+        if(!user.authenticate(password)){
+            return res.status(400).json({
+                error: 'Email and password do not match'
+            })
+        }
+
+        //generate token and send to client
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+        const {_id, name, email, role} = user
+
+        return res.json({
+            token, user: {_id, name, email, role}
+        })
+    } )
+}
--- node-back/routes/auth.js
+++ node-back/routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 //import validators
-const {userRegisterValidator} = require('../validators/auth')
+const {userRegisterValidator, userLoginValidator} = require('../validators/auth')
 const {runValidation} = require('../validators')
 // import from controllers
-const {register, registerActivate} = require('../controllers/auth');
+const {register, registerActivate, login} = require('../controllers/auth');
 
 router.post('/register', userRegisterValidator, runValidation, register);
 router.post('/register/activate', registerActivate);
+router.post('/login', userLoginValidator, runValidation, login);
 
 module.exports = router;
