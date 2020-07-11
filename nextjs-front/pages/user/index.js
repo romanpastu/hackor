c8ad883aa1be34db211f29d4c804873d31958f59
romanpastu <roman.pastushenko.1997@gmail.com>
2020-07-11 18:59:12 +0200

    ssr implementation

--- nextjs-front/pages/user/index.js
+++ nextjs-front/pages/user/index.js
@@ -1,5 +1,16 @@
 import Layout from '../../components/Layout'
+import axios from 'axios'
 
-const User = () => <Layout>Hello User</Layout>
+
+const User = ({todos}) => <Layout>{JSON.stringify(todos)}</Layout>
+
+User.getInitialProps = async() => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+    console.log('SERVER RENDERED', response)
+    return {
+        todos: response.data
+    }
+}
+
 
 export default User;
